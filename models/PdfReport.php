9469c6c85211a7fcf7d976d21d11c6c6d4651e9f
gsoules <gsoules@avantlogic.com>
2020-08-21 13:28:28 -0400

    Update PdfReport.php

--- models/PdfReport.php
+++ models/PdfReport.php
@@ -7,12 +7,23 @@ class PdfReport
         $identifier = ItemMetadata::getItemIdentifier($item);
         $title = ItemMetadata::getItemTitle($item);
         $text = "$identifier: $title";
+        $text = html_entity_decode($text, ENT_QUOTES | ENT_XML1, 'UTF-8');
 
-        $pdf = new FPDF();
+        $imageFileName = '';
+        $itemFiles = $item->Files;
+        if ($itemFiles)
+        {
+            // Remove the first file because it appears in the Primary section above the fields.
+            $imageFileName = FILES_DIR . '/fullsize/' . $itemFiles[0]['filename'];
+        }
+
+
+        $pdf = new FPDF('P', 'in', 'letter');
         $pdf->AddPage();
-        $pdf->SetFont('Arial','B',16);
-        $pdf->Cell(40,10, $text);
-        $pdf->Output('test1.pdf', 'D');
+        $pdf->SetFont('Arial','',12);
+        $pdf->Image($imageFileName);
+        $pdf->Cell(0,0, $text);
+        $pdf->Output('test7.pdf', 'D');
     }
 
     public function createReportForSearchResults()
