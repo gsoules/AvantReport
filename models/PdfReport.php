951d4121cebdeeacf0e0a55efa97037e7b1771f2
gsoules <gsoules@avantlogic.com>
2020-08-21 14:42:12 -0400

    Update PdfReport.php

--- models/PdfReport.php
+++ models/PdfReport.php
@@ -4,33 +4,42 @@ class PdfReport
 {
     public function createReportForItem($item)
     {
-        $identifier = ItemMetadata::getItemIdentifier($item);
-        $title = ItemMetadata::getItemTitle($item);
+        $pdf = new FPDF('P', 'in', 'letter');
 
-        $imageFileName = '';
+        $pdf->SetTopMargin(0.75);
+        $pdf->SetLeftMargin(0.75);
+        $pdf->SetRightMargin(0.75);
+
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','',13);
+
+        $title = ItemMetadata::getItemTitle($item);
+        $pdf->Cell(0, 0, self::decode($title));
+        $pdf->Ln(0.2);
+
         $itemFiles = $item->Files;
         if ($itemFiles)
         {
             // Remove the first file because it appears in the Primary section above the fields.
             $imageFileName = FILES_DIR . '/fullsize/' . $itemFiles[0]['filename'];
+            $pdf->Image($imageFileName);
+            $pdf->Ln(0.2);
         }
 
-        $pdf = new FPDF('P', 'in', 'letter');
-        $pdf->AddPage();
-        $pdf->SetFont('Arial','',13);
-        $pdf->Cell(0, 0, self::decode($title));
-        $pdf->Ln(0.2);
-        $pdf->Image($imageFileName);
-        $pdf->Ln(0.4);
         $pdf->SetFont('Arial','',10);
-        $pdf->Cell(1, 0, 'Identifier:', 0, 0, 'R');
-        $pdf->Cell(0, 0, self::decode($identifier));
-        $pdf->Ln(0.2);
-        $pdf->Cell(1, 0, 'Date:', 0, 0, 'R');
-        $pdf->Cell(0, 0, '2020-08-21');
-        $pdf->Ln(0.2);
-        $pdf->Cell(1, 0, 'Title:', 0, 0, 'R');
-        $pdf->Cell(0, 0, self::decode($title));
+        $elementTexts = get_db()->getTable('ElementText')->findByRecord($item);
+
+        foreach ($elementTexts as $elementText)
+        {
+            $name = ItemMetadata::getElementNameFromId($elementText['element_id']);
+            $text = $elementText['text'];
+            $pdf->SetTextColor(80, 80, 80);
+            $pdf->Cell(1, 0.2, "$name:", 0, 0, 'R');
+            $pdf->SetTextColor(0, 0, 0);
+            $pdf->MultiCell(6, 0.18, self::decode($text));
+            $pdf->Ln(0.08);
+        }
+
         $pdf->Output('test1.pdf', 'D');
     }
 
