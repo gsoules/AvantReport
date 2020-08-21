68f2f7419ebbdafc7bfecc4f51d26bf51cf835cb
gsoules <gsoules@avantlogic.com>
2020-08-21 14:57:55 -0400

    Update PdfReport.php

--- models/PdfReport.php
+++ models/PdfReport.php
@@ -16,25 +16,44 @@ class PdfReport
         $title = ItemMetadata::getItemTitle($item);
         $pdf->Cell(0, 0, self::decode($title));
         $pdf->Ln(0.2);
+        $pdf->SetFont('Arial','',10);
 
         $itemFiles = $item->Files;
         if ($itemFiles)
         {
             // Remove the first file because it appears in the Primary section above the fields.
-            $imageFileName = FILES_DIR . '/fullsize/' . $itemFiles[0]['filename'];
-            $pdf->Image($imageFileName);
+            $primaryImage = $itemFiles[0];
+            $imageFileName = FILES_DIR . '/fullsize/' . $primaryImage['filename'];
+            if ($primaryImage['mime_type'] == 'image/jpeg')
+            {
+                $pdf->Image($imageFileName);
+            }
+            else
+            {
+                $pdf->Ln(0.2);
+                $pdf->Cell(1, 0, "Attachment: $imageFileName");
+            }
             $pdf->Ln(0.2);
         }
 
-        $pdf->SetFont('Arial','',10);
         $elementTexts = get_db()->getTable('ElementText')->findByRecord($item);
 
+        $previousName = '';
         foreach ($elementTexts as $elementText)
         {
             $name = ItemMetadata::getElementNameFromId($elementText['element_id']);
+            if ($name == $previousName)
+            {
+                $name = '';
+            }
+            else
+            {
+                $previousName = $name;
+                $name .= ':';
+            }
             $text = $elementText['text'];
             $pdf->SetTextColor(80, 80, 80);
-            $pdf->Cell(1, 0.2, "$name:", 0, 0, 'R');
+            $pdf->Cell(1, 0.2, $name, 0, 0, 'R');
             $pdf->SetTextColor(0, 0, 0);
             $pdf->MultiCell(6, 0.18, self::decode($text));
             $pdf->Ln(0.08);
